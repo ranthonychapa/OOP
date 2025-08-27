56dd7b45a3c4fce8af38b20e5147568699d4fee5
ranthonychapa <80563698+ranthonychapa@users.noreply.github.com>
2025-08-27 13:56:38 -0500

    Add files via upload

---
+++ Classwork1.py
@@ -0,0 +1,24 @@
+#problem 1
+print("Weclome to OOP Class")
+name = input("Enter your Name: ")
+print("Welcome ", name)
+#problem 2
+student_name = input("Enter the student name:")
+
+course1 = int(input("Enter the grade for course 1: "))
+course2 = int(input("Enter the grade for course 2: "))
+course3 = int(input("Enter the grade for course 3: "))
+course4 = int(input("Enter the grade for course 4: "))
+course5 = int(input("Enter the grade for course 5: "))
+
+total = course1+course2+course3+course4+course5
+print("Your total points is: ", total)
+
+avg = total/5
+print(student_name,"'s average grade is: ", avg)
+#problem 3
+wages = int(input("Enter the wages"))
+hours = int(input("Enter your hours: "))
+days = int(input("Enter your days: "))
+monthly_wages = wages*hours*days
+print(monthly_wages)
